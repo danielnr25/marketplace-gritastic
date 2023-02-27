a1755ecabac24cf9a84ffd8a81fd474de69038b3
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2023-02-26 22:10:18 -0500

    feat:finish-marketplace

--- resources/views/partials/learning/aboutme.blade.php
+++ resources/views/partials/learning/aboutme.blade.php
@@ -14,7 +14,7 @@
                 <img src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_1280,h_700/https://laempresa.online/wp-content/uploads/2020/12/plan-de-empresa-destacada.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
                     <h3 class="card-title">Visión</h3>
-                    <p class="card-text">"Ser una compañía verdaderamente global; brindando a las personas el poder para diseñar y publicar en cualquier lugar."</p>
+                    <p class="card-text">Ser una compañía verdaderamente global; brindando a las personas el poder para diseñar y publicar en cualquier lugar.</p>
                 </div>
             </div>
         </div>
@@ -29,7 +29,7 @@
         </div>
         <div class="col-lg-3">
             <div class="card">
-                <img src="https://www.portafolio.co/files/article_main/uploads/2019/07/05/5d1fedd5ee7dd.jpeg" alt="...">
+                <img src="https://www.portafolio.co/files/article_main/uploads/2019/07/05/5d1fedd5ee7dd.jpeg" alt="..." class="card-img-top">
                 <div class="card-body">
                     <h3 class="card-title">Valores</h3>
                     <li>
@@ -60,6 +60,9 @@
 
 @push('css')
 <style>
+    .card{
+        height: 350px;
+    }
     .card-title {
         font-size: 1.2rem;
         color: #1b2c39;
@@ -74,6 +77,10 @@
         line-height: 1.4;
     }
 
+    .card-img-top{
+        height: 150px;
+        object-fit: cover;
+    }
     .container {
         padding-bottom: 2rem;
     }
